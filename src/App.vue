13ec534d05c484e060fdd4c9aa146f65c3843c76
nawan44 <rachmat.d.gunawan@gmail.com>
2021-01-23 23:31:59 +0700

    about section

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+<About />
   </div>
 </template>
 
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import About from "./components/section/About"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, About
   }
 }
 </script>
---
+++ src/components/section/Client.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>
+		CLIENT
+	</div>
+</template>
